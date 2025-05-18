57435658f8d9069ba9a3434d1dcd5c98b5603ce7
Zohra Tlili <tlili.zohra12@gmail.com>
2025-05-19 00:14:42 +0200

    reg

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.get("/api/items", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("/^/(?!api).*/", (req, res) => {
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
